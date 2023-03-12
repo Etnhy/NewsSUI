f3d9d45fc3108c9d07e2ec12761910064409dca7
Evhenii Mahlena <evhenii.mahlena.dev@gmail.com>
2023-03-12 21:04:48 +0200

    Initial Commit

---
+++ NewsSUI/NewsSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSUIApp.swift
+//  NewsSUI
+//
+//  Created by Евгений Маглена on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
